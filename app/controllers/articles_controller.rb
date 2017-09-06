9497d0f326f0db45061944b21992a47c6bd2fd50
Tristan Hari <kptristan@Tristans-MacBook-Pro.local>
2017-09-06 01:22:38 -0700

    CRUD functions in place

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,24 @@
 class ArticlesController < ApplicationController
+	def index
+		@articles = Article.all
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
 	def new
 	end
 	
 	def create
-		render plain: params[:article].inspect
+		@article = Article.new(params.require(:article).permit(:title, :text))
+ 
+		@article.save
+		redirect_to @article
 	end
-end
+
+	private
+		def article_params
+			params.require(:article).permit(:title, :text)
+		end
+	end
