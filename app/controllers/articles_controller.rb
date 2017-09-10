47f61a809ff5ccfc87a8b6e6fec277648f061222
Tristan Hari <hari.tristan@gmail.com>
2017-09-10 16:19:41 -0700

    Added really shitey atuhentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+
+	http_basic_authenticate_with name: "kptristan", password: "Temp123",
+	except: [:index, :show]
+
 	def index
 		@articles = Article.all
 	end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,8 @@
 class CommentsController < ApplicationController
+
+  http_basic_authenticate_with name: "kptristan", password: "Temp123",
+  only: :destroy
+
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
@@ -11,7 +15,7 @@ class CommentsController < ApplicationController
     @comment.destroy
     redirect_to article_path(@article)
   end
-  
+
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
